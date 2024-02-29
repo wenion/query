aa6ccd343e4d02f786172cea12eb64489e914e39
SteveLEEEEE <yuheng.li@monash.edu>
2024-02-29 17:49:23 +1100

    fix style

--- tutorial/__init__.py
+++ tutorial/__init__.py
@@ -312,7 +312,7 @@ def expert_replay(trace):
         event_description = get_text_by_event("keydown", text_key_down, "")
         trace_message_list.append(f"{event_description}<small>[{pre_url}]</small>")
 
-    trace_message = "<div style='max-height: 500px; overflow-y: auto'><ul><li>" + "</li><li>".join(trace_message_list) + "</li></ul></div>"
+    trace_message = "<div style='max-height: 500px; overflow-y: auto; overflow-x: hidden; boarder: 1.5px solid grey'><ul><li>" + "</li><li>".join(trace_message_list) + "</li></ul></div>"
     return trace_message
 
 def get_keyboard(text_keydown, content):
