d3f4af50af8c7d7a499b97eaac22b310f98bdb8c
SteveLEEEEE <yuheng.li@monash.edu>
2024-06-05 22:01:50 +1000

    update delete pm

--- tutorial/nosql/__init__.py
+++ tutorial/nosql/__init__.py
@@ -551,7 +551,7 @@ def update_process_model(name, creator, update):
 def delete_process_model(name, creator):
     try:
         pm = fetch_process_model_by_name_creator(name, creator)
-        pm.delete()
+        ProcessModel.delete(pm.pk)
     except:
         return False
     else:
