bc37d1011a2527855c6f20c051bd1872018401d9
SteveLEEEEE <yuheng.li@monash.edu>
2024-06-06 11:26:17 +1000

    fix

--- tutorial/__init__.py
+++ tutorial/__init__.py
@@ -218,10 +218,10 @@ def task_classification(request):
     # if interval == 0:
     #     print(user_id + ": Invalid interval")
     #     return {"task_name": "", "certainty": 0, "message": "", "interval": 500}
-    time.sleep(8)
+    time.sleep(3)
     time_delta_in_second = 10
     current_time = datetime.now()
-    time_ago = current_time - timedelta(seconds=10)
+    time_ago = current_time - timedelta(seconds=5)
     time_ago = int(time_ago.timestamp() * 1000)
     result = fetch_all_user_event(user_id, "timestamp")
     #result = fetch_all_user_event_within_time(user_id, time_ago)
