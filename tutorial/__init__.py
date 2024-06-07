29af142253fc04b8960e9fd10faee56d3adc707b
SteveLEEEEE <yuheng.li@monash.edu>
2024-06-07 20:34:30 +1000

    fix time

--- tutorial/__init__.py
+++ tutorial/__init__.py
@@ -222,7 +222,7 @@ def task_classification(request):
         print(user_id + ": Invalid interval")
         return {"task_name": "", "certainty": 0, "message": "", "interval": 5000}
     current_time = datetime.now()
-    time_ago = current_time - timedelta(seconds=7)
+    time_ago = current_time - timedelta(seconds=8)
     time_ago = int(time_ago.timestamp() * 1000)
     result = fetch_all_user_event_within_time(user_id, time_ago)
     trace = pd.DataFrame(result["table_result"])
