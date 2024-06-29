cbc6c829c7043ae4242e906317c11c75e5e3e4e9
SteveLEEEEE <yuheng.li@monash.edu>
2024-06-29 15:04:26 +1000

    exclude google

--- tutorial/__init__.py
+++ tutorial/__init__.py
@@ -268,7 +268,8 @@ def create_pm(request):
             "created": False
         }
     trace = pd.DataFrame(result["table_result"])
-    trace = trace[(trace["tag_name"] != "RECORD") & (~trace["tag_name"].str.startswith("HYPOTHESIS"))] # filter out RECORD events
+    trace = trace[(trace["tag_name"] != "RECORD") & (~trace["tag_name"].str.startswith("HYPOTHESIS"))] # filter out RECORD events and extension events
+    trace = trace[~trace["base_url"].str.contains("docs.google.com")] # exclude google related events; to be removed in actual evaluation TODO
     net, im, fm = create_process_model_from_log(trace)
     if not net:
         return {
