27266950103143e6ae70eef6196c5af39ba9da49
SteveLEEEEE <yuheng.li@monash.edu>
2024-07-23 20:40:03 +1000

    enable PM creation

--- tutorial/__init__.py
+++ tutorial/__init__.py
@@ -237,12 +237,7 @@ def delete_all_tp(request):
 
 @view_config(route_name="create_process_model", request_method="POST", renderer="json")
 def create_pm(request):
-    # disabled during evaluation
-    if True:
-        return {
-            "message": "Disabled during evaluation",
-            "created": False
-        }
+
     if not request.json_body:
         return {
             "message": "Invalid data",
@@ -344,12 +339,6 @@ def create_pm(request):
 
 @view_config(route_name="delete_process_model", request_method="POST", renderer="json")
 def delete_pm(request):
-    # disable functionality for evaluation
-    if True:
-        return {
-            "message": "Disabled during evaluation",
-            "removed": False
-        }
 
     if not request.json_body:
         return {
